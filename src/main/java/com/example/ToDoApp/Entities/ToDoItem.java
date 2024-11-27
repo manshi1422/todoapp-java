8f36d0c1f3ca1d1fe197e122018588a2bade0e25
Manshi <kumarimanshi814@gmail.com>
2024-11-27 22:41:49 +0530

    entity added

---
+++ src/main/java/com/example/ToDoApp/Entities/ToDoItem.java
@@ -0,0 +1,25 @@
+package com.example.ToDoApp.Entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ToDos")
+public class ToDoItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "description")
+    private String description;
+}
